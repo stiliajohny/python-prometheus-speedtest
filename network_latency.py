ece4e4da96628fb8ec57c023b7a43bb1533dab1d
John Stilia <stilia.johny+internal.gitlab.perosnal@gmail.com>
2020-06-30 19:13:21 +0100

    change module  to speed test
     add upload, download and speed

--- network_latency.py
+++ network_latency.py
@@ -1,40 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-15 -*-
 
-from tcp_latency import measure_latency
-import flask
+from flask import Flask
 import speedtest
 from hurry.filesize import size, si
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route('/metrics', methods=['GET'])
 def metrics():
-    st = speedtest.Speedtest()
-    download = size(st.download(), system=si)
-    upload = size(st.upload(), system=si)
-    servernames = []
-    st.get_servers(servernames)
-    latency = st.results.ping
+    s = speedtest.Speedtest()
+    s.get_servers()
+    s.get_best_server()
+    download_speed = s.download()
+    upload_speed = s.upload()
+    ping_speed = str (s.results.ping)
+    return '''
+        python_network_latency_speedtest {ping}</br>
+        python_network_upload_speedtest {upload} </br>
+        python_network_download_speedtest {download}
+        '''.format(upload=upload_speed, download=download_speed, ping=ping_speed)
 
-    print(latency)
-    print(download)
-    print(upload)
-    print(st.results.ping)
 
-    return (
-        "python_network_latency_speedtest " + str(latency) +
-        "\npython_network_upload_speedtest " + str(upload) +
-        "\npython_network_download_speedtest " + str(download) +
-        "\n"
-    )
+@app.route('/', methods=['GET'])
+def root():
+
+    return '''
+        <html>
+        <head>
+        <title>Network Stats Exporter</title>
+        <head>
+        <body>
+        <h1>Network Stats Exporter</h1>
+         <a href="./metrics">Metrics</a>
+         <hr>
+         <h3> Python Application writen in order to export upload, download and ping speeds </br>
+              Author <b> John Stilia </b> </h3>
+        </body>
+        </html>
+        '''
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-app.run(host='0.0.0.0', port=8000)
 
+app.run(host='0.0.0.0', port=8000)
